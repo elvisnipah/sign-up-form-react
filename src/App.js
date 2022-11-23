5df0227a8cbc69fe92b6c3b4d59ec3557b89a6a5
Elvis <elvisnipah@gmail.com>
2022-11-23 08:28:37 +0000

    finished up mobile design

--- src/App.js
+++ src/App.js
@@ -22,53 +22,57 @@ function handleChange(event) {
 }
 
   return (
-    <div className="bg-[url('./images/bg-intro-mobile.png')] flex flex-col grow justify-start items-start p-5 bg-red-400 text-white">
-      <p className="text-2xl p-9 text-center font-bold">
-        Learn to code by watching others
-      </p>
-      <p className="text-center font-bold">
-        See how experienced developers solve problems in real-time. Watching
-        scripted tutorials is great, but understanding how developers think is
-        invaluable.
-      </p>
-      <button className="mt-10 bg-purple-700 py-6 rounded-2xl px-12"><strong>Try it free 7 days</strong> then $20/mo. thereafter</button> 
-      <form action="">
-        <input 
-          type="text" 
-          placeholder="First Name"
-          className=""
-          name="firstName"
-          onChange={handleChange}
-          value={formData.firstName}
-        />
-        <input 
-          type="text" 
-          placeholder="Last Name"
-          className=""
-          name="lastName"
-          onChange={handleChange}
-          value={formData.lastName}
-        />
-        <input 
-          type="email" 
-          placeholder="Email address"
-          className=""
-          name="email"
-          onChange={handleChange}
-          value={formData.email}
-        />
-        <input 
-          type="password" 
-          placeholder="Password"
-          className=""
-          name="password"
-          onChange={handleChange}
-          value={formData.password}
-        />
+    <div className="bg-[url('./images/bg-intro-mobile.png')] flex flex-col sm:flex-row  grow justify-around items-center p-5 bg-red-400 text-white sm:p-10">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-2xl p-8 text-center font-bold sm:text-5xl sm:text-left">
+          Learn to code by watching others
+        </p>
+        <p className="text-center font-bold sm:text-left sm:px-8">
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <button className="mt-10 bg-purple-700 py-6 rounded-2xl px-12 shadow-lg shadow-purple-400 text-center"><strong>Try it free 7 days</strong> then $20/mo. thereafter</button>
+        <form action="" className="mt-10 flex flex-col items-center gap-5 bg-white p-5 rounded-xl">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+            name="firstName"
+            onChange={handleChange}
+            value={formData.firstName}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+            name="lastName"
+            onChange={handleChange}
+            value={formData.lastName}
+          />
+          <input
+            type="email"
+            placeholder="Email address"
+            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+            name="email"
+            onChange={handleChange}
+            value={formData.email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+            name="password"
+            onChange={handleChange}
+            value={formData.password}
+          />
         
-        <button>Claim your free trial</button>
-        By clicking the button, you are agreeing to our Terms and Services
-      </form>
+          <button className="bg-green-400 p-5 rounded-lg font-bold w-[95%] shadow-lg shadow-green-600">CLAIM YOUR FREE TRIAL</button>
+          <p className="text-gray-400 text-center p-2">By clicking the button, you are agreeing to our <span className="font-bold text-red-500">Terms and Services</span></p>
+        </form>
+      </div>
     </div>
   );
 }
