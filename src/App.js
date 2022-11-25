ee54da69e2143eaa1757c27b025e0efc44c9f459
Elvis <elvisnipah@gmail.com>
2022-11-25 12:12:03 +0000

    added form validation logic and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import mobileBg from "./images/bg-intro-mobile.png";
 import desktopBg from "./images/bg-intro-desktop.png";
+import errorIcon from "./images/icon-error.svg";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -9,7 +10,37 @@ function App() {
     lastName: "",
     email: "",
     password: "",
+    submitted: false,
   });
+
+  const firstNameValidation = (firstName) => {
+    if (!firstName && formData.submitted) {
+      return "First Name is required";
+    }
+    return "";
+  };
+
+  function lastNameValidation(lastName) {
+    if (!lastName && formData.submitted) {
+      return "Last Name is required";
+    }
+    return "";
+  }
+
+  function emailValidation(email) {
+    if (!email && formData.submitted) {
+      return "Email is required";
+    } else if (!new RegExp(/\S+@\S+\.\S+/).test(email) && formData.submitted) {
+      return "Incorrect email format";
+    }
+  }
+
+  function passwordValidation(password) {
+    if (!password && formData.submitted) {
+      return "Password is required";
+    }
+    return "";
+  }
 
   function handleChange(event) {
     const { name, value, checked, type } = event.target;
@@ -18,13 +49,20 @@ function App() {
         ...prevFormData,
         //if the type of input is a checkbox, change the checked field. else change the value field
         [name]: type === "checkbox" ? checked : value,
+        submitted: false,
       };
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    console.log(firstNameValidation(formData.firstName));
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        submitted: true,
+      };
+    });
   }
 
   return (
@@ -40,55 +78,101 @@ function App() {
         </p>
       </div>
       <div className="flex flex-col items-center">
-        <button className="mt-10 bg-purple-700 py-6 rounded-2xl px-12 shadow-[0px_7px_0px_0px_rgba(0,0,0,0.2)] text-center cursor-default w-full">
+        <button className="mt-10 bg-purple-700 py-6 rounded-2xl px-12 shadow-[0px_7px_0px_0px_rgba(0,0,0,0.2)] text-center cursor-default w-full ">
           <strong>Try it free 7 days</strong> then $20/mo. thereafter
         </button>
         <form
           action=""
-          className="mt-10 flex flex-col items-center gap-5 bg-white p-5 rounded-xl shadow-[0px_9px_0px_0px_rgba(0,0,0,0.2)]"
+          className="mt-10 flex flex-col items-center gap-5 justify-center bg-white p-5 rounded-xl shadow-[0px_9px_0px_0px_rgba(0,0,0,0.2)]"
           noValidate
           onSubmit={handleSubmit}
         >
-          <input
-            required
-            type="text"
-            placeholder="First Name"
-            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
-            name="firstName"
-            onChange={handleChange}
-            value={formData.firstName}
-          />
-          <input
-            required
-            type="text"
-            placeholder="Last Name"
-            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
-            name="lastName"
-            onChange={handleChange}
-            value={formData.lastName}
-          />
-          <input
-            required
-            type="email"
-            placeholder="Email address"
-            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
-            name="email"
-            onChange={handleChange}
-            value={formData.email}
-          />
-          <input
-            required
-            type="password"
-            placeholder="Password"
-            className="border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
-            name="password"
-            onChange={handleChange}
-            value={formData.password}
-          />
+          <span className="w-full flex flex-col items-center ">
+            <input
+              id="first_name"
+              required
+              type="text"
+              placeholder="First Name"
+              className={
+                lastNameValidation(formData.firstName) && formData.submitted
+                  ? "border-red-500 border-solid border-[1px] text-black p-3 rounded-lg w-[95%] bg-[url('./images/icon-error.svg')] bg-no-repeat bg-[right_0.5rem_top_0.8rem]"
+                  : "border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+              }
+              name="firstName"
+              onChange={handleChange}
+              value={formData.firstName}
+            />
+
+            <p
+              className="italic 
+             self-end pr-4 text-xs text-red-500"
+            >
+              {firstNameValidation(formData.firstName)}
+            </p>
+          </span>
+
+          <span className="w-full flex flex-col items-center">
+            <input
+              required
+              type="text"
+              placeholder="Last Name"
+              className={
+                lastNameValidation(formData.lastName) && formData.submitted
+                  ? "border-red-500 border-solid border-[1px] text-black p-3 rounded-lg w-[95%] bg-[url('./images/icon-error.svg')] bg-no-repeat bg-[right_0.5rem_top_0.8rem]"
+                  : "border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+              }
+              name="lastName"
+              onChange={handleChange}
+              value={formData.lastName}
+            />
+
+            <p className="italic self-end pr-4 text-xs text-red-500">
+              {lastNameValidation(formData.lastName)}
+            </p>
+          </span>
+
+          <span className="w-full flex flex-col items-center">
+            <input
+              required
+              type="email"
+              placeholder="Email address"
+              className={
+                lastNameValidation(formData.email) && formData.submitted
+                  ? "border-red-500 border-solid border-[1px] text-black p-3 rounded-lg w-[95%] bg-[url('./images/icon-error.svg')] bg-no-repeat bg-[right_0.5rem_top_0.8rem]"
+                  : "border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+              }
+              name="email"
+              onChange={handleChange}
+              value={formData.email}
+            />
+            <p className="italic self-end pr-4 text-xs text-red-500">
+              {emailValidation(formData.email)}
+            </p>
+          </span>
+
+          <span className="w-full flex flex-col items-center">
+            <input
+              required
+              type="password"
+              placeholder="Password"
+              className={
+                lastNameValidation(formData.password) && formData.submitted
+                  ? "border-red-500 border-solid border-[1px] text-black p-3 rounded-lg w-[95%] bg-[url('./images/icon-error.svg')] bg-no-repeat bg-[right_0.5rem_top_0.8rem]"
+                  : "border-gray-400 border-solid border-[1px] text-black p-3 rounded-lg w-[95%]"
+              }
+              name="password"
+              onChange={handleChange}
+              value={formData.password}
+            />
+            <p className="italic self-end pr-4 text-xs text-red-500">
+              {passwordValidation(formData.password)}
+            </p>
+          </span>
 
           <button className="bg-green-400 p-5 rounded-lg font-bold w-[95%] shadow-[0px_5px_0px_0px_rgba(0,156,0,0.8)]">
             CLAIM YOUR FREE TRIAL
           </button>
+
           <p className="text-gray-400 text-center p-2">
             By clicking the button, you are agreeing to our{" "}
             <span className="font-bold text-red-500">Terms and Services</span>
